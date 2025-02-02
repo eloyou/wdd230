9f41a0df8f7a400f1997bbbbd103af3894d4db41
eloyou <angelobcaringal@gmail.com>
2025-02-02 22:01:07 +0800

    commit

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -5,3 +5,38 @@ hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('open');
     navigation.classList.toggle('open');
 })
+
+const visits = document.querySelector('.visits-info');
+
+const dateToday = new Date().toLocaleDateString();
+
+const getDate = localStorage.getItem('date-now');
+
+if (getDate){
+
+    const startDate = new Date(getDate);
+    const endDate = new Date(dateToday);
+
+    const differenceInMilliseconds = endDate.getTime() - startDate.getTime();
+    const differenceInDays = differenceInMilliseconds / (1000 * 3600 * 24);
+
+    if (getDate != dateToday) {
+        visits.textContent = "You last visited " + differenceInDays + " days ago"
+    }
+
+    visits.textContent = "Back so soon! Awesome!";
+    
+
+} else {
+
+    localStorage.setItem('date-now', dateToday);
+    visits.textContent = "Welcome! Let us know if you have any questions.";
+
+}
+
+
+
+
+
+
+
